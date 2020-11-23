b908d2c63b8dc67475086b29af7386456b0a5e3c
lhui <963586363lh>
2020-11-23 14:00:15 +0800

    springcloud-学习搭建

---
+++ src/main/java/com/lihui/tsetspringcloud/TsetSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.lihui.tsetspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsetSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsetSpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lihui/tsetspringcloud/TsetSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihui.tsetspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsetSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
