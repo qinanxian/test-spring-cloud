ae5f9db056e2f81e96852b75e7fa0cdfef2bdfb9
lhui <963586363lh>
2020-11-27 16:55:07 +0800

    springcloud-学习搭建

--- service-eureka/src/main/java/com/lihui/serviceeureka/ServiceEurekaApplication.java
+++ service-eureka/src/main/java/com/lihui/serviceeureka/ServiceEurekaApplication.java
@@ -2,8 +2,10 @@ package com.lihui.serviceeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer//开启Eureka服务
 public class ServiceEurekaApplication {
 
     public static void main(String[] args) {
