537b91525627290915c41e26df83a72dde7c001b
963586363@qq.com <963586363@qq.com>
2020-12-07 09:30:12 +0800

    springcloud-学习搭建zuul模块

---
+++ service-zuul/src/test/java/com/object/servicezuul/ServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.object.servicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
